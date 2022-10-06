a284693d9a6fdbec67a6e44457c7fc78c39248d4
mtf360 <metamorfosismarketing360@gmail.com>
2022-10-06 20:48:46 -0300

    agregué archivo server

--- config/server.js
+++ config/server.js
@@ -1,4 +1,11 @@
+// Path: ./config/server.js
+/* 
 module.exports = ({ env }) => ({
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
+}); */
+
+  module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
